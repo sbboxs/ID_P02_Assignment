1a6f52df43d17284935e92b29ea9399bc25e14a1
Sbboxs <ChiaJiunHong@gmail.com>
2021-11-17 20:04:19 +0800

    OMGGG MY NAVIGATION BARR IS WORKINGGG. TIME TO REST. RESPONSIVE WEBSITE PROGRESS 5/100

---
+++ Disaster.js
@@ -0,0 +1,25 @@
+//Function for navigation
+const navSlide= () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click',()=>{
+        //Toggle
+        nav.classList.toggle('nav-active');
+
+        //Animation
+        navLinks.forEach((link,index)=>{
+            if(link.style.animation){
+                link.style.animation='';
+            } else{
+                link.style.animation= `navLinkFade 0.5s ease forwards ${index/7+0.3}s`;
+            }
+        });
+        //Burger Animation
+        burger.classList.toggle('toggle');
+    });   
+
+}
+
+navSlide();
